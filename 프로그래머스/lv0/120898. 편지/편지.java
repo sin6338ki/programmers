d77be1e418573ff4e92f3b30c44303fe9aa1c2b9
신지영 <130349912+sin6338ki@users.noreply.github.com>
2023-07-28 08:45:14 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+//         for(int i = 0 ; i < message.length() ; i++){
+//             answer += 2;
+//         }
+
+        answer = message.toCharArray().length * 2;
+        
+        return answer;
+    }
+}
