949150fffa8760465c0f17831b20d0d8d87c3367
신지영 <130349912+sin6338ki@users.noreply.github.com>
2023-08-10 12:37:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(long num) {
+
+        int answer = 0;
+        
+        while(num!=1){
+
+			if(answer == 500) {
+	        	answer = -1;
+	        	break;
+	        }
+	            
+            answer++;
+
+            if(num%2==0){
+                num /= 2;    
+            }else{
+                num = num*3+1;
+            }
+    
+        }
+
+        return answer;
+    }
+}
