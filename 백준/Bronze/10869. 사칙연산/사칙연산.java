e328bf5887b3515d124fe17dc67b29018d8c1d4f
신지영 <130349912+sin6338ki@users.noreply.github.com>
2023-10-27 14:49:55 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int A = 0;
+        int B = 0;
+        Scanner scanner = new Scanner(System.in);
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        
+        if(A >= 1 && B <= 10000){
+            System.out.println(A+B);
+            System.out.println(A-B);
+            System.out.println(A*B);
+            System.out.println(A/B);
+            System.out.println(A%B);
+        }
+    }
+}
