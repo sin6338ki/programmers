6744450a58186f1b520a84e01ec85eca62048b4c
신지영 <130349912+sin6338ki@users.noreply.github.com>
2024-03-25 22:07:42 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,9 @@
+class Solution {
+    //n : 처음 세균의 마리수
+    //t : 경과한 시간
+    //answer : t시간 후 세균의 수
+    public int solution(int n, int t) {
+        int answer = n << t;
+        return answer;
+    }
+}
