318af2f3e13a088d251d54a417b308cc7002ea3d
신지영 <130349912+sin6338ki@users.noreply.github.com>
2024-05-05 19:35:20 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,14 +1,6 @@
 class Solution {
-    public String solution(String myString) {
-        String newString = myString;
-        String answer = "";
-            
-        for(char c : myString.toCharArray()){
-            if(c < 97){
-                newString = myString.toLowerCase();
-            }
-        }
-        
-        return newString.contains("a") ? newString.replace("a", "A") : newString;
+    public String solution(String myString) {  
+        String answer = myString.toLowerCase();
+        return answer.contains("a") ? answer.replace("a", "A") : answer;
     }
 }
--- 프로그래머스/0/181874. A 강조하기/README.md
+++ 프로그래머스/0/181874. A 강조하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] A 강조하기 - 181874 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874?language=java#) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 0.03 ms
+메모리: 73.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 05일 19:33:23
+2024년 05월 05일 19:35:19
 
 ### 문제 설명
 
