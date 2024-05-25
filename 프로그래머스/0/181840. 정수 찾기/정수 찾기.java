7bc79acb49bdd4d4c70adb7060215683d0adcf7c
신지영 <130349912+sin6338ki@users.noreply.github.com>
2024-05-25 21:15:31 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        
+        for(int i : num_list){
+            if(n == i){
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
