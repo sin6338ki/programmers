b3cbcff912fe5b2b69175aa813d9653d0a45a904
신지영 <130349912+sin6338ki@users.noreply.github.com>
2024-12-24 23:00:53 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int num1 = 0;
-        int num2 = 0;
+        int width = 0;
+        int height = 0;
         int sum = brown + yellow;
         
         for(int i = 1 ; i < brown ; i++){    
-            if(sum % i == 0){
-                 if(i >= sum / i){
-                     num1 = i;
-                     num2 = sum / i;
-                 }
-                
-                if((num1 - 2) * (num2 - 2) == yellow){
-                    answer[0] = num1;
-                    answer[1] = num2;
+            if(sum % i == 0 && i >= sum / i){        
+                width = i;
+                height = sum / i;
+                 
+                if((width - 2) * (height - 2) == yellow){
+                    answer[0] = width;
+                    answer[1] = height;
                 }
             }
         }
