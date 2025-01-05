2ad3656aae42214282118cd8aa9c486b4c05cdce
신지영 <130349912+sin6338ki@users.noreply.github.com>
2025-01-05 23:27:34 +0900

    Update 구명보트.java

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,22 +1,23 @@
 import java.util.Arrays;
 
+/* 풀이
+    1. 몸무게 배열을 오름차순으로 정렬한 후 
+    2. 양 끝 몸무게의 합이 limit 보다 작거나 같은 경우 범위 좁히기 
+*/
 class Solution {
     public int solution(int[] people, int limit) {
         //모든 사람을 구출하기 위해 필요한 구명보트의 개숫의 최솟값
         int answer = 0;
-        Arrays.sort(people);
+        Arrays.sort(people); 
         
         int i = people.length - 1;
         int j = 0;
         while(i >= j){
+            answer++;
             if(people[i] + people[j] <= limit){
-                answer++;
-                j++;
-                i--;
-            }else{
-                answer++;
-                i--;
+                j++;   
             }
+            i--;
         }
         return answer;
     }
